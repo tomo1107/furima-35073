ee1b3d08dbe0decccba363aa1aae32061e04d201
tomo1107 <tfemnrie@gmail.com>
2021-04-08 18:46:22 +0900

    是正対応2回目
    以下のご指摘に対しての是正対応
    https://github.com/tomo1107/furima-35073/pull/2#pullrequestreview-631174222

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
-  FULL_WIDTH_REGEX = /\A[ぁ-んァ-ン一-龥]/.freeze     #全角(ひらがな・カタカタ・漢字)
-  FULL_WIDTH_KATAKANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze     #全角カタカナのみ
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze     #英数字混合
-  
+
+  FULL_WIDTH_REGEX = /\A[ぁ-んァ-ン一-龥]/.freeze # 全角(ひらがな・カタカタ・漢字)
+  FULL_WIDTH_KATAKANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze # 全角カタカナのみ
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze # 英数字混合
+
   validates :password, format: { with: PASSWORD_REGEX, message: 'Include both letters and numbers' }
   with_options presence: true do
     validates :nickname
