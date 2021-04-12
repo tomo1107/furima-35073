6c29954996479a1547f811ab72472279eb256af7
tomo1107 <tfemnrie@gmail.com>
2021-04-12 20:17:35 +0900

    商品出品機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,29 @@
 class Item < ApplicationRecord
+  PRICE_REGEX = /\A[0-9]+\z/.freeze # 半角数字
+
   with_options presence: true do
+    validates :image
     validates :item_name
     validates :description
-    validates :price
-    validates :category_id
-    validates :item_status_id
-    validates :delivery_fee_id
-    validates :prefecture_id
-    validates :date_of_delivery_id
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' },
+                      format: { with: PRICE_REGEX, message: 'Half-width number' }
+    with_options numericality: { other_than: 1, message: 'Select' } do
+      validates :category_id
+      validates :item_status_id
+      validates :delivery_fee_id
+      validates :prefecture_id
+      validates :date_of_delivery_id
+    end
   end
 
   belongs_to :user
   # has_one :purchase
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :Category
+  belongs_to :ItemStatus
+  belongs_to :DeliveryFee
+  belongs_to :Prefecture
+  belongs_to :DateOfDelivery
 end
